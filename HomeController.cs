b3265a5d0558a86c9d25b66f0f42c98aefe12331
N01177393 <31517973+N01177393@users.noreply.github.com>
2017-08-31 17:13:12 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;   
+using System.Web.Mvc;
+using BartenderApp.Models;
+using HtmlAgilityPack;
+
+namespace BartenderApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        
+        private static int id = 0;
+        
+
+        
+        public static List<Drink> Orders = new List<Drink>();
+        
+
+
+        
+        private static List<Drink> drinks
+            = new List<Drink>()
+            {
+                new Drink {Name = "Bronx Cocktail", Description = "Dry and sweet vermouth with orange juice", Price = 10},
+                new Drink {Name = "French Martini", Description = "Raspberry liqueur and pineapple juice", Price = 16},
+                new Drink {Name = "Gimlet", Description = "Lime cordial", Price = 19},
+                new Drink {Name = "Saketini", Description = "Sake instead of vermouth", Price = 18}
+            };
+
+
+        
+        public ActionResult Index() 
+        {
+            
+            return View(drinks);
+        }  
+
+
+        
+        public RedirectToRouteResult Order(Drink drink) 
+        {
+            drink.Id = ++id;
+            Orders.Add(drink);
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
+
---
+++ QueueController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using BartenderApp.Models;
+
+namespace BartenderApp.Controllers
+{
+     public class QueueController : Controller
+    {
+        
+        private static List<Drink> ready = new List<Drink>();
+
+        
+        public ActionResult ViewOrdersTable () 
+
+        {
+            return View(HomeController.Orders);
+        }
+
+       
+       public RedirectToRouteResult Prepared(Drink drink)
+        {
+            ready.Add(drink);
+
+           var drinkToRemove = HomeController.Orders.Where(x => x.Id == drink.Id);      
+            HomeController.Orders.Remove(drinkToRemove.FirstOrDefault());
+
+            return RedirectToAction("ViewOrdersTable");  
+        }
+
+
+        
+        public ActionResult ViewPreparedTable() 
+
+        {
+            return View(ready); 
+        }
+
+        
+        
+        public RedirectToRouteResult Served(Drink drink)
+        {
+            ready.RemoveAll(x => x.Id == drink.Id);
+            return RedirectToAction("ViewPreparedTable");
+        }
+
+    }
+}
